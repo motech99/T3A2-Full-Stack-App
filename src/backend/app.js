f08c25d4ccdace2a880b3579404f7ae4ba291600
xanni99 <xanni99.m@hotmail.com>
2024-08-21 08:42:26 +1000

    re-routed user routes

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,7 +1,8 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
-import { User, HireOption, Equipment, Booking} from './db.js'
+import {HireOption, Equipment, Booking} from './db.js'
+import userRoutes from './routes/user_routes.js'
 
 
 const app = express()
@@ -12,18 +13,7 @@ app.use(express.json())
 
 app.get('/', (req, res) => res.send({ info: 'GC Activity Rentals' }))
 
-// Get Users
-app.get('/users', async (req, res) => res.send(await User.find()))
-
-// Create a new User
-app.post('/users', async (req, res) => {
-    try {
-        const newUser = await User.create(req.body)
-        res.status(201).send(newUser)
-    } catch (err) {
-        res.status(400).send({error: err.message})
-    }
-})
+app.use(userRoutes)
 
 // Get all Hire Options
 app.get('/hireOptions', async (req, res) => res.send(await HireOption.find())) 
