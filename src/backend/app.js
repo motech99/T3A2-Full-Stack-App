de7c631e1536db2bc8221de70879a3e848fac9e4
xanni99 <xanni99.m@hotmail.com>
2024-08-21 08:56:59 +1000

    re-routed booking routes

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,10 +1,10 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
-import {Booking} from './db.js'
 import userRoutes from './routes/user_routes.js'
 import hireOptionsRoutes from './routes/hire_options_routes.js'
 import equipmentRoutes from './routes/equipment_routes.js'
+import bookingRoutes from './routes/booking_routes.js'
 
 
 const app = express()
@@ -15,17 +15,11 @@ app.use(express.json())
 
 app.get('/', (req, res) => res.send({ info: 'GC Activity Rentals' }))
 
-app.use(userRoutes)
-
-app.use(hireOptionsRoutes)
-
-app.use(equipmentRoutes)
-
-
-
-// Get all Bookings
-app.get('/bookings', async (req, res) => res.send(await Booking.find()))
 
 // Routers
+app.use(userRoutes)
+app.use(hireOptionsRoutes)
+app.use(equipmentRoutes)
+app.use(bookingRoutes)
 
 export default app
