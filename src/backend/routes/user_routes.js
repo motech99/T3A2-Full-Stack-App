52b64f0d4a06a943a241919473e0aa7742a3d3a2
xanni99 <xanni99.m@hotmail.com>
2024-08-21 15:45:03 +1000

    added password hashing

--- src/backend/routes/user_routes.js
+++ src/backend/routes/user_routes.js
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import { User } from '../db.js'
+import bcrypt from 'bcrypt'
 
 const router = Router()
 
@@ -7,13 +8,18 @@ const router = Router()
 router.get('/users', async (req, res) => res.send(await User.find()))
 
 // Create a new User
+
 router.post('/users', async (req, res) => {
     try {
-        const newUser = await User.create(req.body)
-        res.status(201).send(newUser)
+        const { password, ...userData } = req.body; 
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+        const newUser = await User.create({ ...userData, password: hashedPassword });
+        res.status(201).send(newUser);
     } catch (err) {
-        res.status(400).send({error: err.message})
+        res.status(400).send({ error: err.message });
     }
-})
+});
+
 
 export default router
