8f65e65839d61c4dd3b87c43c448e4e80c90093b
xanni99 <xanni99.m@hotmail.com>
2024-08-23 21:19:15 +1000

    deploy test

--- src/backend/app.js
+++ src/backend/app.js
@@ -13,7 +13,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.get('/', (req, res) => res.send({ info: 'GC Activity Rentals' }))
+app.get('/', (req, res) => res.send({ info: 'GC Activity Rentals!' }))
 
 
 // Routers
