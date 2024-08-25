08f67622402a247fcf399a76ba5c6dcb5b3ad748
Mohammed Hani <104127533+motech99@users.noreply.github.com>
2024-08-26 02:09:33 +1000

    added tables to the prices page for each equpment

--- src/frontend/src/pages/Prices.jsx
+++ src/frontend/src/pages/Prices.jsx
@@ -1,5 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 import { EQUIPMENT_URL } from './Equipment.jsx';
+import './Prices.css';
 
 export const Prices = () => {
   const { data, isLoading, error } = useQuery({
@@ -11,29 +12,64 @@ export const Prices = () => {
   if (error) return <p>Error: {error.message}</p>;
 
   return (
-    <>
-      <h1 className='title headings'>Equipment</h1>
-      <ul>
-        {data?.map((prices) => (
-          <li key={prices._id}>
-            <h2>{prices.item}</h2>
-            <img
-              src={prices.image}
-              alt={prices.item}
-              style={{ width: '200px', height: 'auto' }}
-            />
-            <p>Quantity: {prices.quantity}</p>
-            <h3>Rates:</h3>
-            <ul>
-              {prices.rates.map((rate) => (
-                <li key={rate._id}>
-                  {rate.hireOption.option} - ${rate.price}
-                </li>
-              ))}
-            </ul>
-          </li>
+    <div className='equipment-container'>
+      <h1 className='title headings has-text-centered'>Prices</h1>
+      <div className='columns is-multiline is-centered'>
+        {data?.map((equipment) => (
+          <div
+            key={equipment._id}
+            className='column is-5-desktop is-8-tablet is-12-mobile equipment-sizing'>
+            <div className='card equipment-card'>
+              <div className='card-image'>
+                <figure className='image is-328x328'>
+                  <img src={equipment.image} alt={equipment.item} />
+                </figure>
+              </div>
+              <div className='card-content'>
+                <div className='media'>
+                  <div className='media-content'>
+                    <h1 className='price-headings has-text-centered'>
+                      {equipment.item.toUpperCase()}
+                    </h1>
+                    <table className='table is-fullwidth mt-4 is-striped table-color is-hoverable'>
+                      <tbody>
+                        <tr>
+                          <th className='price-heading-table'>QUANTITY</th>
+                          <td className='price-heading-table'>
+                            {equipment.quantity}
+                          </td>
+                        </tr>
+                        {equipment.rates.length > 0 && (
+                          <>
+                            <tr>
+                              <th
+                                colSpan='2'
+                                className='has-text-centered price-heading-table'>
+                                RATES
+                              </th>
+                            </tr>
+                            {equipment.rates.map((rate) => (
+                              <tr className='table-hoverable' key={rate._id}>
+                                <td>{rate.hireOption.option}</td>
+                                <td className='pricing-color has-text-weight-semibold'>
+                                  ${rate.price}
+                                </td>
+                              </tr>
+                            ))}
+                          </>
+                        )}
+                      </tbody>
+                    </table>
+                    <button className='mt-4 button is-warning is-fullwidth'>
+                      Book now
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         ))}
-      </ul>
-    </>
+      </div>
+    </div>
   );
 };
