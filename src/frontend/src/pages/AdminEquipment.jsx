3f126e9ec9f53d800eb0358e42ffedfa5d00c634
Mohammed Hani <104127533+motech99@users.noreply.github.com>
2024-08-28 02:52:27 +1000

    fixed styling for adminEquipment and implemented delete function

--- src/frontend/src/pages/AdminEquipment.jsx
+++ src/frontend/src/pages/AdminEquipment.jsx
@@ -1,8 +1,8 @@
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { EQUIPMENT_URL } from './Equipment.jsx';
-import './styles/AdminEquipment.css';
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import './styles/AdminEquipment.css';
 
 export const ManageEquipment = () => {
   const queryClient = useQueryClient();
@@ -17,11 +17,10 @@ export const ManageEquipment = () => {
 
   useEffect(() => {
     if (data) {
-      const initializedData = data.map(equipment => {
+      const initializedData = data.map((equipment) => {
         const ratesMap = {};
 
-        // Populate ratesMap with existing hire options and prices
-        equipment.rates.forEach(rate => {
+        equipment.rates.forEach((rate) => {
           ratesMap[rate.hireOption._id] = rate.price;
         });
 
@@ -30,27 +29,30 @@ export const ManageEquipment = () => {
           item: equipment.item,
           quantity: equipment.quantity,
           rates: ratesMap,
-          hireOptions: equipment.rates.map(rate => ({
+          hireOptions: equipment.rates.map((rate) => ({
             id: rate.hireOption._id,
-            option: rate.hireOption.option
-          }))
+            option: rate.hireOption.option,
+          })),
         };
       });
       setEquipmentData(initializedData);
     }
   }, [data]);
 
-  const mutation = useMutation({
+  const updateMutation = useMutation({
     mutationFn: async (updatedEquipment) => {
       const token = localStorage.getItem('authToken');
-      const response = await fetch(`https://t3a2-full-stack-app-api.onrender.com/equipment/${updatedEquipment._id}`, {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${token}`
-        },
-        body: JSON.stringify(updatedEquipment),
-      });
+      const response = await fetch(
+        `https://t3a2-full-stack-app-api.onrender.com/equipment/${updatedEquipment._id}`,
+        {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+          },
+          body: JSON.stringify(updatedEquipment),
+        }
+      );
 
       if (!response.ok) {
         throw new Error('Error updating equipment');
@@ -82,6 +84,52 @@ export const ManageEquipment = () => {
     },
   });
 
+const deleteMutation = useMutation({
+  mutationFn: async (equipmentId) => {
+    const token = localStorage.getItem('authToken');
+
+    console.log('Deleting equipment with ID:', equipmentId); // Debugging log
+
+    const response = await fetch(
+      `https://t3a2-full-stack-app-api.onrender.com/equipment/${equipmentId}`,
+      {
+        method: 'DELETE',
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    if (!response.ok) {
+      const errorResponse = await response.json();
+      console.error('Error response from server:', errorResponse); // Log error response from server
+      throw new Error(errorResponse.error || 'Error deleting equipment');
+    }
+
+    return response.json();
+  },
+  onMutate: async (equipmentId) => {
+    await queryClient.cancelQueries(['equipment']);
+    const previousData = queryClient.getQueryData(['equipment']);
+
+    queryClient.setQueryData(['equipment'], (oldData) =>
+      oldData.filter((equipment) => equipment._id !== equipmentId)
+    );
+
+    return { previousData };
+  },
+  onError: (err, equipmentId, context) => {
+    queryClient.setQueryData(['equipment'], context.previousData);
+    alert(`Error deleting equipment: ${err.message}`);
+  },
+  onSuccess: () => {
+    alert('Equipment deleted successfully!');
+  },
+  onSettled: () => {
+    queryClient.invalidateQueries(['equipment']);
+  },
+});
+
   const handleFieldChange = (index, hireOptionId, value) => {
     const updatedData = [...equipmentData];
     if (hireOptionId === 'quantity') {
@@ -94,24 +142,29 @@ export const ManageEquipment = () => {
 
   const handleUpdate = (index) => {
     const equipmentToUpdate = equipmentData[index];
-    // Convert rates object to array with hireOption IDs
     const updatedEquipment = {
       ...equipmentToUpdate,
-      rates: Object.keys(equipmentToUpdate.rates).map(hireOptionId => ({
+      rates: Object.keys(equipmentToUpdate.rates).map((hireOptionId) => ({
         hireOption: hireOptionId,
-        price: Number(equipmentToUpdate.rates[hireOptionId])
-      }))
+        price: Number(equipmentToUpdate.rates[hireOptionId]),
+      })),
     };
-    mutation.mutate(updatedEquipment);
+    updateMutation.mutate(updatedEquipment);
+  };
+
+  const handleDelete = (equipmentId) => {
+    if (window.confirm('Are you sure you want to delete this equipment?')) {
+      console.log('Attempting to delete equipment with ID:', equipmentId); // Debugging log
+      deleteMutation.mutate(equipmentId);
+    }
   };
 
   if (isLoading) return <h1 className='title headings'>Loading...</h1>;
   if (error) return <p>Error: {error.message}</p>;
 
-  // Extract hire options for column headers
   const allHireOptions = equipmentData.reduce((acc, equipment) => {
-    equipment.hireOptions.forEach(option => {
-      if (!acc.some(o => o.id === option.id)) {
+    equipment.hireOptions.forEach((option) => {
+      if (!acc.some((o) => o.id === option.id)) {
         acc.push(option);
       }
     });
@@ -119,64 +172,88 @@ export const ManageEquipment = () => {
   }, []);
 
   return (
-    <div className='equipment-container'>
-      <h1 className='title headings has-text-centered mb-6'>Manage Equipment</h1>
-      <button
-        className='button is-primary mb-4'
-        onClick={() => navigate('/equipment/add')}
-      >
-        Add Equipment
-      </button>
-      <div className='card equipment-card'>
-        <div className='card-content'>
-          <table className='table is-fullwidth mt-4 is-striped table-color is-hoverable'>
-            <thead>
-              <tr>
-                <th>Item</th>
-                <th>Quantity</th>
-                {allHireOptions.map(option => (
-                  <th key={option.id}>{option.option}</th>
-                ))}
-                <th>Actions</th>
-              </tr>
-            </thead>
-            <tbody>
-              {equipmentData.map((equipment, index) => (
-                <tr key={equipment._id}>
-                  <td data-label="Item">{equipment.item}</td>
-                  <td data-label="Quantity">
-                    <input
-                      type='number'
-                      value={equipment.quantity}
-                      onChange={(e) => handleFieldChange(index, 'quantity', e.target.value)}
-                    />
-                  </td>
-                  {allHireOptions.map(option => (
-                    <td key={option.id} data-label={option.option}>
-                      <input
-                        type='number'
-                        value={equipment.rates[option.id] || ''}
-                        onChange={(e) => handleFieldChange(index, option.id, e.target.value)}
-                      />
-                    </td>
-                  ))}
-                  <td>
-                    <button
-                      type='button'
-                      className='button is-success'
-                      onClick={() => handleUpdate(index)}
-                    >
-                      Update
-                    </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+    <div className='background-booking'>
+      <div className='equipment-container'>
+        <h1 className='title headings has-text-centered has-text-white mb-6'>
+          MANAGE EQUIPMENT
+        </h1>
+        <div className='columns is-multiline is-centered'>
+          {equipmentData.map((equipment, index) => (
+            <div
+              key={equipment._id}
+              className='column is-5-desktop is-8-tablet is-12-mobile equipment-sizing'>
+              <div className='card equipment-card'>
+                <div className='card-content m-2'>
+                  <div className='media'>
+                    <div className='media-content'>
+                      <h2 className='title is-4 has-text-centered login-heading login-border'>
+                        {equipment.item.toUpperCase()}
+                      </h2>
+
+                      <div className='field'>
+                        <label className='label'>Quantity</label>
+                        <div className='control'>
+                          <input
+                            type='number'
+                            className='input'
+                            value={equipment.quantity}
+                            onChange={(e) =>
+                              handleFieldChange(
+                                index,
+                                'quantity',
+                                e.target.value
+                              )
+                            }
+                          />
+                        </div>
+                      </div>
+
+                      {allHireOptions.map((option) => (
+                        <div className='field' key={option.id}>
+                          <label className='label'>{option.option}</label>
+                          <div className='control'>
+                            <input
+                              type='number'
+                              className='input'
+                              value={equipment.rates[option.id] || ''}
+                              onChange={(e) =>
+                                handleFieldChange(
+                                  index,
+                                  option.id,
+                                  e.target.value
+                                )
+                              }
+                            />
+                          </div>
+                        </div>
+                      ))}
+
+                      <div className='field is-grouped'>
+                        <div className='control'>
+                          <button
+                            type='button'
+                            className='button is-success'
+                            onClick={() => handleUpdate(index)}>
+                            Update
+                          </button>
+                        </div>
+                        <div className='control'>
+                          <button
+                            type='button'
+                            className='button is-danger'
+                            onClick={() => handleDelete(equipment._id)}>
+                            Delete
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
   );
 };
-
-
