96aa6f9a82ff1ff9323f886f7ef2db718f7c8de0
Mohammed Hani <104127533+motech99@users.noreply.github.com>
2024-08-28 23:39:57 +1000

    deploying

--- src/frontend/vite.config.js
+++ src/frontend/vite.config.js
@@ -1,11 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'esnext', // or 'modules' depending on your needs
+    outDir: 'dist', // Default output directory for production builds
+  },
   test: {
     environment: 'jsdom',
-    globals: true
-  }
-})
+    globals: true,
+  },
+});
